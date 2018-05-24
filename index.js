3cdefa9f3f852b2ecb598b03c941bba681cd9ff6
Matthew Smyth <msmyth1030@gmail.com>
2018-05-24 18:07:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,25 +1,33 @@
 var kittens = ['Milo', 'Otis', 'Garfield'];
 function destructivelyAppendKitten(name) {
+  var kittens = ['Milo', 'Otis', 'Garfield'];
   return kittens.push(name);
 }
 function destructivelyPrependKitten(name) {
+  var kittens = ['Milo', 'Otis', 'Garfield'];
   return kittens.unshift(name);
 }
 function destructivelyRemoveLastKitten() {
+  var kittens = ['Milo', 'Otis', 'Garfield'];
   return kittens.pop();
 }
 function destructivelyRemoveFirstKitten() {
+  var kittens = ['Milo', 'Otis', 'Garfield'];
   return kittens.shift();
 }
 function appendKitten(name) {
+  var kittens = ['Milo', 'Otis', 'Garfield'];
   return [...kittens, name];
 }
 function prependKitten(name) {
+  var kittens = ['Milo', 'Otis', 'Garfield'];
   return [name, ...kittens];
 }
 function removeLastKitten() {
+  var kittens = ['Milo', 'Otis', 'Garfield'];
   return kittens.slice(0, kittens.length - 1);
 }
 function removeFirstKitten() {
+  var kittens = ['Milo', 'Otis', 'Garfield'];
   return kittens.slice(1, kittens.length);
 }
